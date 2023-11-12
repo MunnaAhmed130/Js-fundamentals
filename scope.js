33ad90f6ad90bc816c5af7a00e20b3b739ba5252
Munna Ahmed <munnaahmed2025@gmail.com>
2023-11-12 12:40:18 +0600

    scope

--- scope.js
+++ scope.js
@@ -3,16 +3,18 @@ var foo = "global";
 function funcFoo() {
     if (true) {
         // block of code
-        var foo = "local";
+        var foo = "local-foo";
+        bar = "local-bar"; // declaration without keyword variable becomes global
     }
-    console.log(foo); // var is not block scoped
+    return foo; // var is not block scoped
 }
 
 console.log(funcFoo());
 console.log(foo); // var is function scoped
+console.log(bar);
 
 if (true) {
-    var foo = "local";
+    var foo = "local-foo";
 }
 
 console.log(foo); // var is not block scoped
