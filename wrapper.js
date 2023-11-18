7ce7ed20a1ccb3b955b469c67e2fda7ac24bfba7
Munna Ahmed <munnaahmed2025@gmail.com>
2023-11-18 18:29:43 +0600

    commit

--- wrapper.js
+++ wrapper.js
@@ -1,2 +1,16 @@
 let str = "My name is Adam";
-console.dir(str);
+let strLength = str.length;
+console.log(strLength);
+
+// console.log("Hello".length);
+
+// behind the scene
+// new String(), wrapper function, a constructor function, creates object
+let strObj = new String(str);
+strLength = strObj.length;
+strObj = null;
+
+console.dir(strObj);
+
+// when you use object property or method on a
+console.log(str.length === new String(str).length);
