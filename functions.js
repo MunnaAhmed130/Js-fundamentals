6a369c4fb477959d3748af113baebae0f72e3e11
Munna Ahmed <munnaahmed2025@gmail.com>
2023-11-19 05:25:03 +0600

    commit

--- functions.js
+++ functions.js
@@ -1,11 +1,19 @@
 function greet(name) {
-  console.log(arguments);
+    console.log(arguments);
 }
 greet("john"); // {}
 
+// arrow functions don't have arguments property
+const greet2 = (greetings) => {
+    // uncaught referenceError: arguments is not defined
+    console.log(arguments);
+};
+
+greet2("Hello");
+
 var result = 0;
 function addFive(result) {
-  return (result += 5);
+    return (result += 5);
 }
 
 console.log(result); // output 0
--- this.js
+++ this.js
@@ -4,19 +4,20 @@
 // const value = 10;
 
 this.value = 10; // global variable with this
-console.log(this); // window object
-console.log(value);
+console.log("global", this); // window object
+// console.log(value);
 
-// in strict mode this in a function is undefined
+// in strict mode this in a function definition is undefined
 function context() {
-    "use strict";
+    // "use strict";
     console.log(this);
 }
 
 context();
 
+// arrow function will always refer to global object
 const func = () => {
-    console.log(this); // window object
+    console.log(this);
 };
 
 func();
@@ -29,17 +30,28 @@ var obj = {
         salary: 5000,
         designation: "officer",
         about: function () {
+            console.log(this);
             console.log(
                 "He is a",
                 this.designation,
                 "with a salary of " + this.salary
             );
         },
+        // arrow function will always refer to global object
+        more: () => {
+            console.log(this);
+        },
     },
     log: function () {
+        console.log(this);
+    },
+    // arrow function will always refer to global object
+    arrow: () => {
         console.log(this);
     },
 };
 
 obj.log();
-// obj.job.about();
+obj.arrow();
+obj.job.about();
+obj.job.more();
