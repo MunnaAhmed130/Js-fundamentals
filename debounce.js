950384786fa57ee8ff7d654082b1c74b68e8bde0
Munna Ahmed <munnaahmed2025@gmail.com>
2023-12-10 22:00:40 +0600

    commit

--- debounce.js
+++ debounce.js
@@ -1,11 +1,11 @@
 const initApp = () => {
     const button = document.querySelector(".btn");
-    // button.addEventListener("click", debounce(clickLog, 2000));
-    button.addEventListener("click", () => {
-        clickLog();
-        button.disabled = true;
-        setTimeout(() => (button.disabled = false), 2000);
-    });
+    button.addEventListener("click", debounce(clickLog, 2000));
+    // button.addEventListener("click", () => {
+    //     clickLog();
+    //     button.disabled = true;
+    //     setTimeout(() => (button.disabled = false), 2000);
+    // });
 };
 
 const clickLog = () => console.log("clicked");
@@ -13,11 +13,13 @@ const clickLog = () => console.log("clicked");
 document.addEventListener("DOMContentLoaded", initApp);
 
 // debounce prevents functions from being called too soon
+// Debouncing is a technique that delays the execution of a function until a certain period of inactivity has passed.
+// decorator function
 const debounce = (fn, delay) => {
     let id;
     console.log(`id at immediate load: ${id}`);
     return (...args) => {
-        console.log(args);
+        // console.log(args);
         console.log(`previous id ${id}`);
         if (id) {
             clearTimeout(id);
--- throttle.js
+++ throttle.js
@@ -1,8 +1,24 @@
 const initApp = () => {
     const tButton = document.querySelector(".tbtn");
-    tButton.addEventListener("click", clickLog);
+    tButton.addEventListener("click", throttle(clickLog, 2000));
+    window.addEventListener("scroll", throttle(scrollLog, 200));
 };
 
 const clickLog = () => console.log("clicked");
+const scrollLog = () => console.log("scrolled");
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const throttle = (fn, delay) => {
+    let lastTime = 0;
+    console.log("called Throttle Immediately");
+    let id = 0;
+    return (...args) => {
+        const now = new Date().getTime();
+        ++id;
+        if (now - lastTime < delay) return;
+        console.log(`event id:${id}`);
+        lastTime = now;
+        fn(...args);
+    };
+};
