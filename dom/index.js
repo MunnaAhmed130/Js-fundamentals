8981a99d4669289f14546beb7b92f775cf34dd2f
Munna Ahmed <munnaahmed2025@gmail.com>
2023-12-15 18:49:54 +0600

    commit

--- dom/index.js
+++ dom/index.js
@@ -1,1 +1,13 @@
-console.log(document);
+// innerText
+// textContent
+// innerHTML
+
+const p = document.querySelector("p");
+
+console.log("innerText =", p.innerText); // not formatted, no tags
+console.log("textContent =", p.textContent); // formatted without tags
+console.log("innterHTML =", p.innerHTML); // formatted with tags
+
+// p.innerHTML = "<strong>Hello</strong>";
+p.textContent = "<strong>Hello</strong>";
+p.innerText = "<strong>Hello</strong>";
---
+++ dom/selectors.js
@@ -0,0 +1,24 @@
+console.log(document);
+
+// selectors
+document.getElementById("h1").innerHTML = "Hey World!";
+// getElementById is method, demo is element, innerHTML is property
+const p = document.getElementsByTagName("p"); // finds all elements with tag name
+// console.log(p);
+document.getElementsByClassName("intro"); // finds all elements with class name
+// document.querySelector(); // returns the first element
+// find all that matches a specific css selector
+// document.querySelectorAll("p.intro");
+
+// all list item
+const li = document.querySelectorAll("li");
+console.log(li);
+const listArr = [...li].map((item) => item.innerText);
+console.log(listArr);
+
+// specific list item
+const ul = document.querySelector("#fruit");
+console.log(ul.querySelectorAll("li"));
+
+const list = [...ul.querySelectorAll("li")].map((item) => item.textContent);
+console.log(list);
