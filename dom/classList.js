494114422ef60903cf8a442178bfa286b8b12e61
Munna Ahmed <munnaahmed2025@gmail.com>
2023-12-16 16:39:19 +0600

    commit

---
+++ dom/classList.js
@@ -0,0 +1,16 @@
+// const first = document.querySelector(".first");
+// const second = document.querySelector(".second");
+// const third = document.querySelector(".third");
+
+// console.log(first.innerText);
+// console.log(second.textContent);
+// console.log(third.innerHTML);
+
+const h1 = document.querySelector("h1");
+// classList - add, remove, toggle
+// h1.classList.add("heading1");
+console.log(h1.classList);
+// h1.classList.remove("heading1");
+h1.classList.toggle("heading1");
+
+console.log(h1.classList);
--- dom/index.js
+++
@@ -1,7 +0,0 @@
-// const first = document.querySelector(".first");
-// const second = document.querySelector(".second");
-// const third = document.querySelector(".third");
-
-// console.log(first.innerText);
-// console.log(second.textContent);
-// console.log(third.innerHTML);
