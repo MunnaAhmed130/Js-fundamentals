c2ca1558cac63fb0323c8c488325ac754759e630
Munna Ahmed <munnaahmed2025@gmail.com>
2024-07-25 02:36:18 +0600

    built in reference type in js

---
+++ basics/builtInTypes.js
@@ -0,0 +1,9 @@
+// Built in Reference types
+// instantiate them with new operator
+
+const items = new Array();
+const now = new Date();
+const error = new Error("Something bad happened.");
+const func = new Function("console.log('Hi');");
+const object = new Object();
+const re = new RegExp("\\d+");
