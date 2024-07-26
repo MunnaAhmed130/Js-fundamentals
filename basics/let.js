3c63b22709ccddee654d83d765931d0f7cbc188b
Munna Ahmed <munnaahmed2025@gmail.com>
2024-07-26 16:27:38 +0600

    changed error message structure to better understand errors

--- basics/let.js
+++ basics/let.js
@@ -5,14 +5,14 @@
 try {
   console.log(first);
 } catch (err) {
-  console.log(err.message); // referneceError: first is not defined
+  console.log(err.name, ":", err.message); // referneceError: first is not defined
 }
 
 // Because temporal dead zone variable cannot be accessed before initialization
 try {
   console.log(greet); // ReferenceError: Cannot access 'greet' before initialization
 } catch (err) {
-  console.log(err.message);
+  console.log(err.name, ":", err.message);
 }
 
 // let is block scoped
@@ -20,7 +20,7 @@ let greet; // end of temporal dead zone
 // because the variable is implicitly initialized with undefined
 
 // after variable declaration/the end of the temporal dead zone the variable is accessible
-console.log("'greet' after implicit initialization, The end of the TDZ", greet);
+console.log("'greet' after implicit initialization, The end of the TDZ", greet); // undefined
 
 // so cannot redeclare
 // SyntaxError: Identifier 'greet' has already been declared
@@ -31,25 +31,23 @@ console.log("'greet' after implicit initialization, The end of the TDZ", greet);
 // can re-assign value
 greet = "Hey Hi!";
 
-if (true) {
-  //  var greet = "hey"; // can not redeclare with var in blocked scope
+try {
+  if (true) {
+    // var greet = "hey"; // can not redeclare with var in blocked scope
 
-  // temporal zone inside of a code block
-  // console.log("block scoped let variable", greet); // cannot access greet before initialization
+    // temporal zone inside of a code block
+    console.log("block scoped let variable", greet); // cannot access greet before initialization
 
-  let greet = "hey";
-  console.log("block scoped let variable", greet);
+    let greet = "hey";
+    console.log("block scoped let variable", greet);
+  }
 
-  // below is an implicit global
-  // greet = ""; // this shouldn't be done use strict mode to avoid this mistake
+  if (false) {
+    // below is an implicit global
+    greet = ""; // this shouldn't be done use strict mode to avoid this mistake
+  }
+} catch (err) {
+  console.log(err.name, ":", err.message);
 }
 
 console.log("re-assigned value -", greet);
-
-// temporal zone inside of a code block
-try {
-  console.log(greet);
-  let greet = "";
-} catch (err) {
-  console.log("TDZ in a code block", err.message);
-}
