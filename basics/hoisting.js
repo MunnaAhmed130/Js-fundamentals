b5b5597f7f772dc75cf54a713b08155d97de379b
Munna Ahmed <munnaahmed2025@gmail.com>
2024-07-31 02:45:09 +0600

    modify

--- basics/hoisting.js
+++ basics/hoisting.js
@@ -1,7 +1,20 @@
 console.log(counter); // undefined
 var counter = 1;
 console.log(counter); // 1
+
+try {
+  console.log(number);
+} catch (err) {
+  console.log(err.name + ":", err.message);
+}
+
 //ReferenceError: Cannot access 'counter' before initialization
-console.log(count);
+
+try {
+  console.log(count);
+} catch (err) {
+  console.log(err.name + ":", err.message);
+}
+
 let count = 1;
 console.log(count); // 1
