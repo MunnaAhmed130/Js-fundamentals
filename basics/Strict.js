21210d763bd2a0744fb3fb50e36dbb55d646ab7e
Munna Ahmed <munnaahmed2025@gmail.com>
2024-08-24 23:32:34 +0600

    more about strict mode

--- basics/Strict.js
+++ basics/Strict.js
@@ -1,5 +1,7 @@
 "use strict"; // es5
 
+// In strict mode
+// variable declaration without a keyword is not allowed
 // name = "";
 
 // In strict mode, `this` is `undefined` in global function declaration
