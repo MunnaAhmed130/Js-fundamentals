4bb058e64cf6d0d3c9b14c967411d86f01551e58
Munna Ahmed <munnaahmed2025@gmail.com>
2024-08-25 00:52:46 +0600

    clean code and better examples

--- functional/call&apply.js
+++ functional/call&apply.js
@@ -1,54 +1,33 @@
 function fullName() {
-    console.log("fullName", this.firstName + " " + this.lastName);
+  console.log("fullName", this.firstName + " " + this.lastName);
 }
 
 function heal(num1, num2) {
-    this.health += num1 + num2;
-    console.log("Health", this);
+  this.health += num1 + num2;
+  console.log("Health", this);
 }
 
 const person = {
-    firstName: "John",
-    lastName: "doe",
-    health: 100,
-    // object method
-    fullName: function () {
-        console.log(this.firstName + " " + this.lastName);
-    },
-    heal: function (num1, num2) {
-        this.health += num1 + num2;
-        console.log(this);
-    },
+  firstName: "Mary",
+  lastName: "doe",
+  health: 50,
 };
 
 const person1 = {
-    firstName: "Mary",
-    lastName: "doe",
-    health: 50,
+  firstName: "John",
+  lastName: "doe",
+  health: 58,
 };
 
 // console.log(Object.getOwnPropertyDescriptor(person, `firstName`));
-
-console.log("function prototype - call");
-
-// method invoke
-person.fullName();
-person.heal(10, 20);
+// console.log("function prototype - call");
 
 // call
-person.fullName.call(person1); // using method of person for person1
-person.heal.call(person1, 10, 20);
+// Typically used when you know the exact number of arguments and can pass them individually.
+fullName.call(person);
+heal.call(person, 10, 20);
 
 // apply
-person.fullName.apply(person1);
-person.heal.apply(person1, [10, 20]);
-
-console.log("function call");
-
-// call
-fullName.call(person1);
-heal.call(person1, 10, 20);
-
-// apply
+// Convenient when the arguments are already in an array or array-like object
 fullName.apply(person1);
 heal.apply(person1, [10, 20]);
