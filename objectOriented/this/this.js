f4d33ed762ea1dc17a56676dc19c541f9612e273
Munna Ahmed <munnaahmed2025@gmail.com>
2024-08-29 04:31:01 +0600

    found something new again - funcion declaration and arrow function in a method behaves different

--- objectOriented/this/this.js
+++ objectOriented/this/this.js
@@ -27,7 +27,21 @@ let person = {
   // getThis: this,
 
   return: function () {
+    let self = this;
     console.log(this);
+    // function declaration inside a object method returns window object
+    function greet() {
+      console.log(this, self);
+    }
+    greet();
+    // function declaration also returns the window object
+    (function () {
+      console.log(this, self);
+    })();
+
+    // arrow function returns this received from closures
+    (() => console.log(this, self))();
+
     return {
       // in this object, returned by method,
       // the value of this is lexically found value from the method
@@ -37,7 +51,7 @@ let person = {
 };
 
 console.log(person.return());
-console.log(person.sayHealth());
+// console.log(person.sayHealth());
 
 const person1 = {
   firstName: "Mary",
@@ -55,29 +69,35 @@ const person1 = {
   sayHealth: person.sayHealth,
 };
 
-console.log(person1.sayHealth());
 // console.log(Object.getOwnPropertyDescriptor(person, `firstName`));
 
-// method invoke
-console.log(person.fullName());
-console.log(fullName.call(person1));
+// console.log(person1.sayHealth());
+// // method invoke
+// console.log(person.fullName());
+// console.log(fullName.call(person1));
+// console.log(person.heal(10, 20));
+// console.log(person1.heal(10, 30));
+// console.log(person1.sayHealth());
 
-console.log(person.heal(10, 20));
-console.log(person1.heal(10, 30));
-
-console.log(person1.sayHealth());
 // -------------------------------------------------------------------
 
+// this example only talks about behaviour in a GEC to learn about behaviour inside a object see above example
 // `this` does not point to the function itself or its lexical scope (with one exception for arrow functions).
 // in a closure or FEC, the inner functions get the value of `this` lexically
 function greet() {
   // "use strict";
   let parentThis = this;
-  // console.log(this);
+  function hello() {
+    "use strict";
+    console.log("hello", this);
+  }
+  hello();
+  console.log("greet", this);
   return function () {
     // console.log(this);
+
     let childThis = this;
-    console.log(parentThis, childThis);
+    console.log("arrow this", parentThis, childThis);
     return parentThis === childThis;
   };
 }
@@ -143,7 +163,7 @@ var cars = [
   { make: "Ferrari", made: "Italia" },
 ];
 
-let vehicle = {
+var vehicle = {
   cars: [
     {
       make: "Lamborghini",
@@ -159,9 +179,38 @@ let vehicle = {
   },
 };
 
-console.log(vehicle.fullName());
-
 // node => error, run in the browser
 console.log(vehicle.fullName.call(this));
+console.log(vehicle.fullName.bind(this)());
 
+console.log(vehicle.fullName());
 // ---------------------------------------------------
+
+var vehicle = {
+  cars: [
+    { make: "Lamborghini", model: "Huracán" },
+    { make: "Mclaren", model: "720s" },
+    { make: "Ferrari", model: "Italia" },
+  ],
+  brand: "lamborghini",
+
+  fullName: function () {
+    let self = this;
+    console.log(this);
+    // A variable inside a function cannot access variables and properties outside its scope so the value of this would be the same inside the inner functions as the outer function called fullName
+    // each function gets access to the 'this' value from fullName through closures
+    this.cars.forEach(function () {
+      console.log(self, car);
+    });
+
+    // this.cars.forEach((self) => {
+    //   console.log(self.make + " " + self.model);
+    // });
+
+    this.cars.forEach((car) => {
+      console.log(car.make + " " + car.model + " " + this.brand);
+    });
+  },
+};
+
+vehicle.fullName();
